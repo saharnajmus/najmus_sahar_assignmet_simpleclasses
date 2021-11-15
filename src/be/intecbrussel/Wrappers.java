2b9e2f9d0e4b05c2d5b998da1c3740fc0244a50b
saharnajmus <saharnajmus91@gmail.com>
2021-11-15 11:27:57 +0100

    if-else statement added

--- src/be/intecbrussel/Wrappers.java
+++ src/be/intecbrussel/Wrappers.java
@@ -12,13 +12,20 @@ public class Wrappers {
         System.out.println(Integer.max(90, 900));
         //Assignment 1 part 2
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a string value");
-        String input = keyboard.next();
-        //Assignment 1 part 3,4,5,6
-        Integer convertToInt = Integer.parseInt(input);
-        System.out.println(convertToInt);
-        System.out.println("Number of bits used to represent an integer value is " + Integer.SIZE);
-        System.out.println("Number of bytes used to represent an integer value is " + Integer.BYTES);
+
+        System.out.println("Enter a number");
+        if(keyboard.hasNextInt()){
+            String input = keyboard.next();
+            System.out.println(input+input);
+            System.out.println("The integer value of your input is" + Integer.valueOf(input));
+            System.out.println("The bit value of your input is" + Integer.bitCount(Integer.parseInt(input)));
+            System.out.println("The integer value of your input is" + Integer.valueOf(input).byteValue());
+
+
+        }
+        else{
+            System.out.println("Try again. This time just press int number");
+        }
 
 
     }
